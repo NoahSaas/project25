977c8c0a7729e51cbbc3008b1eee9d133b63a1ad
NoahSaas <151681167+NoahSaas@users.noreply.github.com>
2025-01-17 10:50:23 +0100

    .

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 require 'sinatra'
 require 'slim'
 require 'sqlite3'
-require 'sinatra/reloader'
-require 'becrypt'
+require 'bcrypt'
 
-hhh
+enable :sessions
+
+get('/') do
+  slim(:index)
+end
+
+get('/products') do
+  db = SQLite3::Database.new('db/database.db')
+  db.results_as_hash = true
+  @products = db.execute('SELECT * FROM products')
+  slim(:products)
+end
